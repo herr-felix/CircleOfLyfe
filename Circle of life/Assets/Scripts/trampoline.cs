3e63b2a3aecabb5d4131e553b35dbff0814afdf7
Alexandre Charron <charronalexandre@hotmail.com>
2016-01-30 08:53:50 -0500

    worked on level

---
+++ Circle of life/Assets/Scripts/trampoline.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trampoline : MonoBehaviour {
+
+    public float force;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Rigidbody playerRb = col.gameObject.GetComponent<Rigidbody>();
+            playerRb.AddForce(new Vector3(0, force, 0), ForceMode.Impulse);
+        }
+    }
+}
