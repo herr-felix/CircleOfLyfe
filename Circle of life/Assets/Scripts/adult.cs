359b5449ace74645b3f498ac32627452466cfbd4
Alexandre Charron <charronalexandre@hotmail.com>
2016-01-30 12:25:08 -0500

    added human controller

---
+++ Circle of life/Assets/Scripts/adult.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class adult : MonoBehaviour {
+
+    bool isWalking = false;
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    float horizontal = Input.GetAxis("Horizontal");
+        if (horizontal > 0) isWalking = true;
+        else isWalking = false;
+
+        if (Input.GetButton("Jump"))
+            anim.SetBool("Jump", true);
+	}
+}
