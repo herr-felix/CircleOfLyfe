58367631d6d7889460497ace51a6678beafa337f
Johnny Interiano <benjonant@msn.com>
2016-01-30 06:40:14 -0500

    blablna

---
+++ Circle of life/Assets/playerBehave.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerBehave : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//use the movement scripts too
+
+		if(Input.GetButton("Action1")){
+			//turn into human form
+		}
+		if (Input.GetButton ("Action2")) {
+			//turn into moose
+		}
+		if (Input.GetButton ("Action3")) {
+			//turn into shark
+		}
+		if (Input.GetButton ("Action4")) {
+			//turn into eagle
+		}
+		if (Input.GetButton ("Action5")) {
+			//turn into pheonix
+		}
+		if(Input.GetButton("Action6")){
+			//turn into shaman?
+		}
+	
+	}
+
+	enum StageOfLife {egg, child, teen, adult, shaman};
+	enum Form {human, moose, shark, eagle, pheonix};
+}
---
+++ Circle of life/Assets/playerMethods.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CollectibleItem : MonoBehaviour
+{
+
+}
+
+public class playerMethods : MonoBehaviour {
+
+
+	public static int collectibleCount = 0;
+
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	public bool IsEarthRitualDone ()
+	{
+		return collectibleCount == 10;
+	}
+
+	public bool IsWaterRitualDone ()
+	{
+		return collectibleCount == 20;
+	}
+
+	public bool IsAirRitualDone ()
+	{
+		return collectibleCount == 30;
+	}
+
+	public bool IsFireRitualDone ()
+	{
+		return collectibleCount == 40;
+	}
+}
+
+
