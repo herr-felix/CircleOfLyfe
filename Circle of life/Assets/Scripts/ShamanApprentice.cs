fad8b53bd64361022d8eee2136947f37b592a6a3
Kevin Avignon <kevin.o.avignon@gmail.com>
2016-01-30 13:32:39 -0500

    Merge

---
+++ Circle of life/Assets/Scripts/ShamanApprentice.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class ShamanApprentice : MonoBehaviour
+{
+    private float _moveSpeed;
+    //private Rigidbody _rb;
+    private Dictionary<CollectibleType, List<Collectible>> _ritualCollectibles;
+    public ShamanSpiritualForm Form;
+
+    //Collect items and store them foreach independant rituals
+    public void CollectItem(CollectibleType ct, Collectible c)
+    {
+        if (_ritualCollectibles.ContainsKey(ct))
+        {
+            _ritualCollectibles[ct].Add(c);
+        }
+        else
+        {
+            _ritualCollectibles.Add(ct, new List<Collectible> { c });
+        }
+    }
+
+    private void MoveSpiritualForm()
+    {
+        if (Form == ShamanSpiritualForm.Egg)
+        {
+            var horizontal = Input.GetAxis("Horizontal");
+            //_rb.AddTorque(new Vector3(0, 0, horizontal*_moveSpeed), ForceMode.Force);
+        }
+        else if (Form == ShamanSpiritualForm.Horse)
+        {
+        }
+        else if (Form == ShamanSpiritualForm.Eagle)
+        {
+            var moveDir = Vector3.zero;
+            moveDir.x = Input.GetAxis("Horizontal"); // get result of AD keys in X
+            transform.position += moveDir * 55f * Time.deltaTime;
+        }
+        else if (Form == ShamanSpiritualForm.Shark)
+        {
+        }
+        else if (Form == ShamanSpiritualForm.Phoenix)
+        {
+        }
+    }
+
+    public void ElevateToSpiritual(string form)
+    {
+        switch (form)
+        {
+            case "Action 1":
+                break;
+        }
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+       // _rb = GetComponent<Rigidbody>();
+        Form = ShamanSpiritualForm.Eagle;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        MoveSpiritualForm();
+    }
+}
+
+public enum CollectibleType
+{
+    EarthCollectible,
+    WindCollectible,
+    FireCollectible,
+    WaterCollectible
+}
+public enum ShamanSpiritualForm
+{
+    Egg,
+    Horse,
+    Phoenix,
+    Shark,
+    Eagle
+}
