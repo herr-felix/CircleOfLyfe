48bc5c4e111dd666b656521d727ae62fbed4eb8a
Alexandre Charron <charronalexandre@hotmail.com>
2016-01-30 02:54:36 -0500

    Added a few things

---
+++ Circle of life/Assets/Scripts/camera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera : MonoBehaviour {
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player");
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.transform.position + new Vector3(0, 5, 20);
+        transform.LookAt(player.transform.position);
+
+	}
+}
---
+++ Circle of life/Assets/Scripts/egg.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class egg : MonoBehaviour {
+
+    public float speed;
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        rb.AddTorque(new Vector3(0, 0, horizontal * speed), ForceMode.Force);
+
+	}
+}
