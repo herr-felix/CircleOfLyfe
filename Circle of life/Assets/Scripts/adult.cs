652485012a06cbb7268d1cdeeca88e9bbedc1a12
Alexandre Charron <charronalexandre@hotmail.com>
2016-01-31 03:41:14 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Circle of life/Library/CurrentMaximizeLayout.dwlt
    #	Circle of life/Library/ScriptAssemblies/Assembly-CSharp.dll
    #	Circle of life/Library/ScriptAssemblies/Assembly-CSharp.dll.mdb
    #	Circle of life/Library/assetDatabase3

--- Circle of life/Assets/Scripts/adult.cs
+++ Circle of life/Assets/Scripts/adult.cs
@@ -4,22 +4,51 @@ using System.Collections;
 public class adult : MonoBehaviour {
 
     bool isWalking = false;
+    bool isJumping = false;
     Animator anim;
+    Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
         anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        //anim.SetBool("Jump", false);
+
 	    float horizontal = Input.GetAxisRaw("Horizontal");
-        if (horizontal != 0) isWalking = true;
+
+        if (horizontal != 0)
+        {
+            isWalking = true;
+            rb.velocity = new Vector3(-5 * horizontal, rb.velocity.y, 0);
+            if (horizontal < 0)
+            {
+                transform.forward = new Vector3(0, 0, -1);
+            }
+            else
+            {
+                transform.forward = new Vector3(0, 0, 1);
+            }
+        }
         else isWalking = false;
 
         anim.SetBool("isWalking", isWalking);
 
-        if (Input.GetButton("Jump"))
+        if (Input.GetButton("Jump") && !isJumping)
+        {
+            isJumping = true;
             anim.SetBool("Jump", true);
+            rb.AddForce(new Vector3(horizontal, 7, 0), ForceMode.Impulse);
+        }
 	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        isJumping = false;
+        anim.SetBool("Jump", false);
+    }
 }
