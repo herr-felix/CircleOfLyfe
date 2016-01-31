550a92d2e8a1fecec530392ce52fa8eeef61e024
Johnny Interiano <benjonant@msn.com>
2016-01-31 02:07:28 -0500

    i have nothing to commit

---
+++ Circle of life/Assets/Scripts/mictest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mictest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		foreach (string device in Microphone.devices) {
+			Debug.Log ("Name: " + device);
+	
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
