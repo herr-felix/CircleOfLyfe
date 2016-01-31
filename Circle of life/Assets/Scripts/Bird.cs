bed6fcfbbd8a5cd411b7105f60a03b30681da57d
Alexandre Charron <charronalexandre@hotmail.com>
2016-01-31 05:34:45 -0500

    added a few scripts

---
+++ Circle of life/Assets/Scripts/Bird.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bird : MonoBehaviour {
+
+    Animator anim;
+    bool isFlapping = false;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        isFlapping = Input.GetButton("Jump");
+        anim.SetBool("isFlapping", isFlapping);
+	}
+}
